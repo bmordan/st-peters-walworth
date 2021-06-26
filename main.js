af301a5bce5586ad4cb7e5c40d14653098f4f82e
bmordan <bernardmordan@gmail.com>
2021-06-26 22:22:35 +0100

    Peace be with you...

---
+++ main.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'pug')
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.render('home'))
+app.get('/services', (req, res) => res.render('services'))
+app.get('/safeguarding', (req, res) => res.render('safeguarding'))
+
+app.listen('3000', () => {
+    console.log(`St Peters Walworth\nstarted: ${new Date().toLocaleString('en-GB')}`)
+})
