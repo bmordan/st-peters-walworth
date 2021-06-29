560f8edef5adcc0891e85e3a0f56dcf6a59971a6
bmordan <bernardmordan@gmail.com>
2021-06-29 09:15:14 +0100

    first pages

--- main.js
+++ main.js
@@ -8,7 +8,9 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => res.render('home'))
 app.get('/services', (req, res) => res.render('services'))
+app.get('/special-services', (req, res) => res.render('special-services'))
 app.get('/safeguarding', (req, res) => res.render('safeguarding'))
+app.get('/clergy', (req, res) => res.render('clergy'))
 
 app.listen('3000', () => {
     console.log(`St Peters Walworth\nstarted: ${new Date().toLocaleString('en-GB')}`)
